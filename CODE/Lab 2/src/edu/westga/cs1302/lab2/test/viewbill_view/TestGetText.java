dc02967076ceb782022c61ccb1d1b24561e2c487
Toochukwu Gabriel Umegakwe <tumegak1@my.westga.edu>
2025-09-02 09:33:55 -0400

    Adding test files

---
+++ CODE/Lab 2/src/edu/westga/cs1302/lab2/test/viewbill_view/TestGetText.java
@@ -0,0 +1,14 @@
+package edu.westga.cs1302.lab2.test.viewbill_view;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestGetText {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
