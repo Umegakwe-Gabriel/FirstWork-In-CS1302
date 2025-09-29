d6de3224da23871375446c6a62447bb03e660b87
Toochukwu Gabriel Umegakwe <tumegak1@my.westga.edu>
2025-09-29 08:00:00 -0400

    2B- Task Manager Class
    Create Task Manager class to store tasks in the observable lists and provided add/get methods.
    Worked on many erorrs during this process.

---
+++ CODE/Project1/src/main/java/edu/westga/cs1302/project1/model/TaskManager.java
@@ -0,0 +1,46 @@
+package edu.westga.cs1302.project1.model;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+
+/**
+ * Manages a list of tasks.
+ * 
+ * @author CS 1302
+ * @version Fall 2025
+ */
+public class TaskManager {
+
+    private final ObservableList<Task> tasks;
+
+    /**
+     * Creates a new TaskManager with an empty task list.
+     */
+    public TaskManager() {
+        this.tasks = FXCollections.observableArrayList();
+    }
+
+    /**
+     * Adds a task to the list.
+     * 
+     * @param task the task to add
+     * @precondition task != null
+     * @postcondition task is added to tasks
+     */
+    public void addTask(Task task) {
+        if (task == null) {
+            throw new IllegalArgumentException("Task cannot be null");
+        }
+        this.tasks.add(task);
+    }
+
+    /**
+     * Returns the observable list of tasks.
+     * 
+     * @return the list of tasks
+     */
+    public ObservableList<Task> getTasks() {
+        return this.tasks;
+    }
+}
+
