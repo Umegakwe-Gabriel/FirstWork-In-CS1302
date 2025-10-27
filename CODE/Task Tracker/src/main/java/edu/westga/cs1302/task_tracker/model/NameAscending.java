b522f113edb0f036f63d226a4e9a567dda1deb7c
Toochukwu Gabriel Umegakwe <tumegak1@my.westga.edu>
2025-10-26 23:05:49 -0400

    2A- Add name-based comparators and clarify prioirty labels
    Introduce NameAscending and NameDescending comparators for alphabetical sorting.
    Update Ascending/Descending toString() to clearly indicate the priority order presented in the UI.

---
+++ CODE/Task Tracker/src/main/java/edu/westga/cs1302/task_tracker/model/NameAscending.java
@@ -0,0 +1,51 @@
+package edu.westga.cs1302.task_tracker.model;
+
+import java.util.Comparator;
+
+/**
+ * Comparator that sorts task by name in ascending (A->Z) order
+ * 
+ * @author CS 1302
+ * @version Fall 2025
+ */
+public class NameAscending implements Comparator<Task> {
+	
+	@Override
+	public int compare(Task aa, Task bb) {
+		if (aa == null && bb == null) {
+			return 0;
+		}
+		if (aa == null) {
+			 return -1;
+		}
+		if (bb == null) {
+			 return 1;
+		}
+		
+		String n1;
+		if (aa.getName() == null) {
+			n1 = "";
+		} else {
+			n1 = aa.getName();
+		}
+		
+		String n2;
+		if (bb.getName() == null) {
+			n2 = "";
+		} else {
+			n2 = bb.getName();
+		}
+		
+		int cmp = n1.compareToIgnoreCase(n2);
+		if (cmp != 0) {
+			return cmp;
+		}
+		
+		return Integer.compare(aa.getPriority().getValue(), bb.getPriority().getValue());
+	}
+	
+	@Override
+	public String toString() {
+		return "Name (A->Z)";
+	}
+}
---
+++ CODE/Task Tracker/src/main/java/edu/westga/cs1302/task_tracker/model/NameDescending.java
@@ -0,0 +1,51 @@
+package edu.westga.cs1302.task_tracker.model;
+
+import java.util.Comparator;
+
+/**
+ * Comparator that sorts tasks by name in descending (Z->A) order
+ * 
+ * @author CS 1302
+ * @version Fall 2025
+ */
+public class NameDescending implements Comparator<Task> {
+	
+	@Override
+	public int compare(Task aa, Task bb) {
+		if (aa == null && bb == null) {
+			return 0;
+		}
+		if (aa == null) {
+			return 1;
+		}
+		if (bb == null) {
+			return -1;
+		}
+		
+		String n1;
+		if (aa.getName() == null) {
+			n1 = "";
+		} else {
+			n1 = aa.getName();
+		}
+		
+		String n2;
+		if (bb.getName() == null) {
+			n2 = "";
+		} else {
+			n2 = bb.getName();
+		}
+		
+		int cmp = n2.compareToIgnoreCase(n1);
+		if (cmp != 0) {
+			return cmp;
+		}
+		
+		return Integer.compare(aa.getPriority().getValue(), bb.getPriority().getValue());
+	}
+	
+	@Override
+	public String toString() {
+		return "Name (Z->A)";
+	}
+}
