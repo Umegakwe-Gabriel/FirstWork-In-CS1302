ad942430d7e72853377672ae93e04c1d02a110c3
Toochukwu Gabriel Umegakwe <tumegak1@my.westga.edu>
2025-12-08 19:15:05 -0500

    Create collection model class for storing collection data.
    Created Collection class with methods to add/remove comics.
    Includes a name and a comics list to hold comic books.

---
+++ CODE/Project3/src/main/java/edu/westga/cs1302/contact_manager/model/Collection.java
@@ -0,0 +1,40 @@
+package edu.westga.cs1302.contact_manager.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Represents a collection of comics in the system.
+ * 
+ * This class is used to store and manage all the comics added by the user.
+ * It provides methods to add, remove, and retrieve comics from the collection.
+ * 
+ * @author CS 1032
+ * @version FALL 2025
+ */
+public class Collection {
+	private String name;
+	private List<Comic> comics;
+	
+	public Collection(String name) {
+		this.name = name;
+		this.comics = new ArrayList<>();
+	}
+	
+	public String getName() {
+		return name;
+	}
+	
+	public List<Comic> getComics() {
+		return comics;
+	}
+	
+	public void addContact(Comic comic) {
+		comics.add(comic);
+	}
+	
+	public void removeComic(Comic comic) {
+		comics.remove(comic);
+	}
+	
+}
